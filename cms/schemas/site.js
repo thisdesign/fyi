9df0c8496be7a9e6f6379a8a51049342471f8868
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-12-31 13:06:45 -0800

    update 'site settings'

--- cms/schemas/site.js
+++ cms/schemas/site.js
@@ -1,3 +1,50 @@
+export const seoDescription = {
+  name: "description",
+  title: "Description",
+  type: "text",
+  rows: 2,
+  description:
+    "Enter a description to get a better ranking on search engines like Google. 50–160 characters reccommended",
+
+  validation: (Rule) =>
+    Rule.max(160).warning(
+      "Descriptions between 50–160 characters reccommended for SEO purposes"
+    ),
+};
+
+export const seoTitle = {
+  name: "title",
+  title: "Page Title",
+  type: "string",
+  validation: (Rule) => Rule.required(),
+  description:
+    "The page displays in the browser's tab and when the page is shared on social media.",
+};
+
+export const seoImage = {
+  title: "Social sharing image",
+  description:
+    "When you share a link to your store on social media, an image is usually shown in your post.",
+  name: "metaImage",
+  type: "image",
+  options: {
+    hotspot: true,
+  },
+};
+
+const TEXT = {
+  title: "Text",
+  name: "text",
+  type: "array",
+  of: [
+    {
+      type: "block",
+      styles: [],
+      lists: [],
+    },
+  ],
+};
+
 export default {
   type: "document",
   name: "site",
@@ -9,9 +56,90 @@ export default {
       validation: (Rule) => Rule.required(),
     },
 
+    /**
+     * General
+     */
+    {
+      name: "general",
+      type: "object",
+      description: "General site settings",
+
+      fields: [
+        {
+          type: "file",
+          name: "backgroundVideo",
+        },
+        {
+          type: "string",
+          name: "contactEmail",
+        },
+      ],
+    },
+
+    /**
+     * Intro
+     */
+    {
+      name: "preload",
+      type: "object",
+
+      fields: [
+        {
+          name: "preloadHead",
+          type: "text",
+          rows: 3,
+        },
+        {
+          name: "preloadBody",
+          type: "text",
+          rows: 3,
+        },
+        {
+          title: "Latitude",
+          name: "lat",
+          type: "number",
+        },
+        {
+          title: "longitude",
+          name: "lng",
+          type: "number",
+        },
+      ],
+    },
+
+    /**
+     * Home
+     */
+    {
+      name: "home",
+      type: "object",
+
+      fields: [
+        {
+          ...TEXT,
+          name: "intro",
+          description: "Homepage large introduction text",
+        },
+        {
+          ...TEXT,
+          name: "paragraph",
+          description: "Homepage paragraph text",
+        },
+        {
+          type: "file",
+          name: "video",
+        },
+      ],
+    },
+
+    /**
+     * Inspiration
+     */
+
     {
       name: "inspiration",
       type: "object",
+
       fields: [
         {
           name: "documents",
@@ -27,6 +155,23 @@ export default {
           ],
         },
       ],
+    },
+
+    /**
+     * SEO
+     */
+
+    {
+      name: "seo",
+      title: "SEO Options",
+      type: "object",
+      fields: [seoTitle, seoDescription, seoImage],
+      description:
+        "Shows up in search previews, social media, and share links. ",
+      options: {
+        collapsable: true,
+        collapsed: false,
+      },
     },
   ],
 };
