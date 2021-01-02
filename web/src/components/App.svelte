468c7c2fcbb62cdcc82a8149df4de021da59344e
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-01-02 11:37:54 -0800

    start SEO

--- web/src/components/App.svelte
+++ web/src/components/App.svelte
@@ -2,6 +2,7 @@
   import type { Site } from '../types'
   import Nav from './Nav.svelte'
   import Preload from './Preload.svelte'
+  import Seo from './Seo.svelte'
 
   export let data: Site
 
@@ -9,6 +10,8 @@
 
   $: heading = data.preload?.preloadHead || null
   $: body = data.preload?.preloadBody || null
+
+  $: baseTitle = data.seo?.title || 'FYi Design'
 </script>
 
 <style type="text/scss">
@@ -16,5 +19,6 @@
   // @import '../style/breakpoints.scss';
 </style>
 
+<Seo pageTitle={null} {baseTitle} />
 <Nav>{lat}°N, {lng}°W</Nav>
 <Preload {heading} {body} />
---
+++ web/src/components/Seo.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let pageTitle: string | null
+  export let baseTitle: string | null
+
+  let title = pageTitle ? `${pageTitle} – ${baseTitle}` : baseTitle
+</script>
+
+<style type="text/scss">
+  // @import '../style/vars.scss';
+  // @import '../style/breakpoints.scss';
+</style>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
