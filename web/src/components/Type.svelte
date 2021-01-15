9c953a28551c77e71837fe7739f125de7ad4cca8
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-01-14 17:25:12 -0800

    massage type intro

--- web/src/components/Type.svelte
+++ web/src/components/Type.svelte
@@ -11,10 +11,11 @@
     let split = new SplitText(el, { type: 'words' })
 
     gsap.from(split.words, {
+      delay: 0.2,
       duration: 0.5,
-      y: 10,
+      y: 15,
       opacity: 0,
-      stagger: 0.025,
+      stagger: 0.02,
       ease: 'Power4.out',
       onComplete: () => split.revert(),
     }),
